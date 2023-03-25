e1fccbc4b55f1d4abca9ebc787f15f0145dbaee0
Abdurahman AYDIN <hariysu@gmail.com>
2023-03-25 12:38:00 +0300

    Changes the project architecture

---
+++ lib/screens/sign_up_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../widgets/sign_up_form.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: const Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: SignUpForm(),
+          ),
+        ),
+      ),
+    );
+  }
+}
